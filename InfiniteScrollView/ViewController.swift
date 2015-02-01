9f8d6c33b61d5cb192fd9c62caddbf7972656b9a
Alex Bechmann <alexbechmann@Alexs-MBP.home>
2015-02-01 14:09:20 +0100

    Initial Commit

---
+++ InfiniteScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfiniteScrollView
+//
+//  Created by Alex Bechmann on 01/02/2015.
+//  Copyright (c) 2015 Alex Bechmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
