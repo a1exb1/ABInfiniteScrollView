1269ba3c8f40c9e91856d093c88112f1d8ce8304
a1exb1 <alexbechmann@Alexs-MBP.home>
2015-02-06 00:12:33 +0100

    first commit

---
+++ InfiniteScrollView/InfiniteSubclassExample.swift
@@ -0,0 +1,33 @@
+//
+//  InfiniteSubclassExample.swift
+//  InfiniteScrollView
+//
+//  Created by Alex Bechmann on 01/02/2015.
+//  Copyright (c) 2015 Alex Bechmann. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteSubclassExample: ABInfiniteScrollView {
+    
+    var i = 50
+    
+    override func infiniteScrollViewDidScroll(direction: ABInfiniteScrollDirection) {
+        for view in self.views{
+            //do what you want with hidden views        
+        }
+        
+        direction == .Left ? i-- : i++
+    }
+    
+    override func infiniteScrollViewNewView(frame:CGRect, pageFrame:CGRect) -> UIView {
+        var rc = UIView(frame: frame)
+        rc.backgroundColor = UIColor.whiteColor()
+        var label = UILabel(frame:pageFrame)
+        label.text = String(i)
+        label.textAlignment = NSTextAlignment.Center
+        rc.addSubview(label)
+        return rc
+    }
+
+}
--- InfiniteScrollView/ViewController.swift
+++ InfiniteScrollView/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var scrollView:InfiniteSubclassExample? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //scrollView?.setTranslatesAutoresizingMaskIntoConstraints(false)
+        scrollView = InfiniteSubclassExample(frame: self.view.bounds)
+        
+        //scrollView.add
+        
+        self.view.addSubview(scrollView!)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
